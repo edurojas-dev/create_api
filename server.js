f5d076594c8b540439bca41883ec681b7573edcd
Eduardo Rojas <61126545+edurojas-dev@users.noreply.github.com>
2021-08-03 16:40:18 -0300

    update file

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 const api = express()
-
 const treinos = [
     { tipo: "Dorsal", descricao: "Puxada barra fixa: 4 x falha" },
     { tipo: "Dorsal", descricao: "Puxada frente: 4 x 12 comb. puxada invertida (supinado)" },
     { tipo: "Dorsal", descricao: "Remada unilateral c/ halter: 4 x 15" },
     { tipo: "Dorsal", descricao: "Cadeira abdutora= 4 x 15 obs: controlar velocidade." },
     { tipo: "Pernas", descricao: "Panturrilha maquina= 4 x 15 execução lenta e concentrada." },
-
 ];
 
 const treinoDorsal = treinos.filter(o => o.tipo === "Dorsal");
@@ -28,7 +26,6 @@ api.get("/treino/pernas", (req, res) => {
 api.get("/treino/dorsal", (req, res) => {
   return res.json(treinoDorsal);
 });
-
 
 api.listen(4000, (error)=>{
     if(error){console.log(error)}
